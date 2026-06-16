16f753ad8321d46a881e53680de7d25d3d41bea1
Витана <Okiloki1111@gmail.com>
2026-06-16 21:26:53 +0300

    Update script.js

--- homework-1/script.js
+++ homework-1/script.js
@@ -29,15 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Dark mode on double click
-  let clicks = 0;
   document.addEventListener('dblclick', () => {
-    clicks++;
-    if (clicks === 2) {
-      const root = document.documentElement;
-      const isDark = root.getAttribute('data-theme') === 'dark';
-      root.setAttribute('data-theme', isDark ? 'light' : 'dark');
-      clicks = 0;
-    }
-    setTimeout(() => clicks = 0, 500);
+    const root = document.documentElement;
+    const isDark = root.getAttribute('data-theme') === 'dark';
+    
+    root.setAttribute('data-theme', isDark ? 'light' : 'dark');
   });
 });
